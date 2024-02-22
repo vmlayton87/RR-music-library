182ecf523cea2c9eed56ce20a020b70cb3c7e78b
vmlayton87 <vmlayton87@gmail.com>
2024-02-22 18:44:18 -0500

    createContext and useContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import './App.css'
+import { DataContext } from './context/DataContext'
+
+
 
 function App(){
     let [search, setSearch] = useState('')
@@ -35,7 +38,10 @@ function App(){
         <div className='App'>
             <SearchBar handleSearch={handleSearch}/>
             {message}
-            <Gallery data={data}/>
+            <DataContext.Provider value={data}>
+            <Gallery />
+            </DataContext.Provider>
+            
         </div>
     )
 }

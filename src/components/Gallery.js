75f168b8e277452859229572884833b1b660a0e1
vmlayton87 <vmlayton87@gmail.com>
2024-02-24 13:41:28 -0500

    with router no context

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,11 @@
 // This will format and display our gallery item components.
 
-import { useContext } from 'react'
-import { DataContext } from '../context/DataContext'
+
 import GalleryItem from './GalleryItem'
 
-function Gallery(){
-    const data = useContext(DataContext)
-    const displayItem = data.map((item, index) => {
+function Gallery(props){
+    
+    const displayItem = props.data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,20 @@
 // This component should take a user input and modify state at the App level to drive results from our fetch.
 
                 
-import { useContext } from 'react'
-import { SearchContext } from '../context/SearchContext'
+import { useState } from 'react'
 
-function SearchBar() {
-    let {term, handleSearch} = useContext(SearchContext)
+function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form >
-            <input ref={term} type="text" placeholder="Enter a search term here" />
-            <button type="submit" onClick={e=>handleSearch(e, term.current.value)}>Submit</button>
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here" onChange={
+                (e) => setSearchTerm(e.target.value)
+            } />
+
+            <input type="submit" />
+
         </form>
     )
 }
